299afc207cd2807bac942a6c59d6aa5d26567f87
murtajizgreen <murtajiz1998@gmail.com>
2021-08-27 18:00:12 +0500

    learning javascript

---
+++ table.js
@@ -0,0 +1,29 @@
+var student = [
+    {
+        "Student_name": "Murtajiz",
+        "Student_Age ": " 23 ",
+        "Student_ID": "041",
+        "DEGREE": " CS "},
+        {
+            "Student_name": "Hamza",
+            "Student_Age ": " 20 ",
+            "Student_ID": "111",
+            "DEGREE": " CS "},
+            {
+                "Student_name": "Saif",
+                "Student_Age ": " 22 ",
+                "Student_ID": "047",
+                "DEGREE": " CS "},
+                {
+                    "Student_name": "Ali",
+                    "Student_Age ": " 23 ",
+                    "Student_ID": "045",
+                    "DEGREE": " BEE "},
+    ]
+function CreateTable(student) {
+  for (items in student)
+    {
+        console.log(items)
+    }
+}
+       
